1eb61cc078a35de607cc4194473b00f69bd4af02
Muhammad Akif Hussain <makifhussainmn@gmail.com>
2024-12-14 14:25:32 +0500

    1 commit

---
+++ first.js
@@ -0,0 +1,16 @@
+let menu =  document.querySelector('#menu-icon');
+let navlist1 = document.querySelectorAll('.NAVLIST-2');
+
+menu.onclick = () => {
+   menu.classList.toggle('bx-x');
+  navlist1.classList.toggle(open)
+}
+    
+    
+//     document.getElementById("menu-icon").onclick = function (){
+//         menu.classList.toggle('bx-x');
+//         let navlist1 = document.querySelectorAll(".navlist");
+//         navlist1.classList.toggle('.open')
+
+
+// }
